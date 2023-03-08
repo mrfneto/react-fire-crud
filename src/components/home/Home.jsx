9db65060fb90596e6ce3fdac2b4067a834582258
Mrfneto <mrfneto@gmail.com>
2023-03-07 21:33:21 -0300

    Delete

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,20 +1,26 @@
 import './Home.css'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { collection, getDocs, onSnapshot, query } from 'firebase/firestore'
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
 import { db } from '../../firebase'
+
 export const Home = () => {
   const [projects, setProjects] = useState([])
   const projectCollection = collection(db, 'projects')
 
+  const get = async () => {
+    const snapshot = await getDocs(projectCollection)
+    setProjects(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+  }
   useEffect(() => {
-    const get = async () => {
-      const snapshot = await getDocs(projectCollection)
-      setProjects(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
-    }
-
     get()
   }, [])
+
+  const remove = async id => {
+    await deleteDoc(doc(db, 'projects', id))
+    setProjects(projects.filter(p => p.id !== id))
+    console.log('Deleted')
+  }
 
   return (
     <div className="home">
@@ -42,8 +48,11 @@ export const Home = () => {
               {projects.map(project => (
                 <tr key={project.title}>
                   <td>{project.title}</td>
-                  <td>
+                  <td className="table__actions">
                     <Link to={`${project.id}/update`}>Edit</Link>
+                    <a type="button" onClick={() => remove(project.id)}>
+                      Delete
+                    </a>
                   </td>
                 </tr>
               ))}
